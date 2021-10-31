7576d5756efbd798d2cd997c760d23e4ef01bc81
Ayesha Uzair <ayesha.shafqat92@gmail.com>
2021-10-31 23:03:09 +0100

    Docstrings added

--- data/process_data.py
+++ data/process_data.py
@@ -4,6 +4,23 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    """
+    This function takes the filepath of the disaster messages and 
+    department categories in csv formats and merges them with respect to ids.
+
+    Parameters
+    ----------
+    messages_filepath : TYPE str
+        DESCRIPTION. filepath for messages csv file
+    categories_filepath : TYPE str
+        DESCRIPTION. filepath for categories csv file
+
+    Returns
+    -------
+    df : TYPE object
+        DESCRIPTION. merged pandas dataframe
+
+    """
     # Read in file
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
@@ -12,6 +29,24 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
+    """
+    This function cleans the dataframe by performing multiple tasks.
+    1. Values in the category column are split on ";" character so 
+    each value becomes a seperate column.
+    2. Columns are renamed using the values from the first row.
+    3. Binary values are assigned to all category columns and replaced in the dataset
+
+    Parameters
+    ----------
+    df : TYPE object
+        DESCRIPTION. Dataframe object
+
+    Returns
+    -------
+    df : TYPE object
+        DESCRIPTION. Cleaned Dataframe object
+
+    """
     # Clean data
     categories_new = df['categories'].str.split(';', expand=True)
     row = list(categories_new.iloc[0])
@@ -24,6 +59,9 @@ def clean_data(df):
     
         # Convert column from string to numeric
         categories_new[column] = pd.to_numeric(categories_new[column])
+        
+    # Few labels of 2 exist so they are replaced with 1
+    categories_new.related.replace(2,1,inplace=True) 
     
     # Drop old categories column and append cleaned one
     df = df.drop('categories', axis=1)
@@ -36,6 +74,22 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    """
+    This function saves the cleaned dataframe and creates a database by
+    taking the database filename as input and stores the dataframe as a table.
+
+    Parameters
+    ----------
+    df : TYPE object
+        DESCRIPTION. Dataframe object
+    database_filename : TYPE str
+        DESCRIPTION. Name of the created database
+
+    Returns
+    -------
+    None.
+
+    """
     # load to database
     # DB name used: 'DisasterResponseSp.db'
     engine = create_engine('sqlite:///' + database_filename )
